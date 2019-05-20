bad3e2d484626bf4aca354ad4634ff8b3ef7aa8b
Laura Guzelj Blatnik <laura.guzelj-blatnik@student.fmf.uni-lj.si>
2019-05-20 14:11:06 +0200

    delali na projektu

---
+++ auth.py
@@ -0,0 +1,4 @@
+db = 'sem2019_klarag'
+host = 'baza.fmf.uni-lj.si'
+user = 'klarag'
+password = '1hy9j2s5'
--- recepti.py
+++ recepti.py
@@ -2,76 +2,116 @@
 import auth
 auth.db = "sem2019_%s" % auth.user
 
-#prijava na bazo, (z sqlite3):
-#conn = sqlite3.connect('...')
-#curzor :
-#cur = conn.cursor()
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
 import csv
-import sqlite3
 
 def ustvari_uporabnik():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS uporabnik(
-            id SERIAL NOT NULL,
+            id SERIAL PRIMARY KEY,
             ime TEXT NOT NULL,
-            skor NUMERIC NOT NULL
+            skor NUMERIC NOT NULL,
+            geslo TEXT NOT NULL
         """)
     conn.commit()
 
 def ustvari_sestavina():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS sestavina(
-            id SERIAL PRIMARY KEY NOT NULL,
-            ime TEXT NOT NULL
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL,
+            enota TEXT
     """)
     conn.commit()
 
 def ustvari_vrsta():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS vrsta(
-            id SERIAL PRIMARY KEY NOT NULL,
-            kategorija TEXT NOT NULL
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL
     """)
     conn.commit()
 
 def ustvari_priloznost():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS priloznost(
-            id SERIAL PRIMARY KEY NOT NULL,
-            priloznost TEXT NOT NULL
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL
     """)
     conn.commit()
 
 def ustvari_priprava():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS priprava(
-            id SERIAL PRIMARY KEY NOT NULL
-            priprava TEXT NOT NULL
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL
     """)
-    conn.commit()    
+    conn.commit()
+
+    
+def ustvari_komentar():
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS komentar (
+            id SERIAL PRIMARY KEY,
+            avtor INTEGER NOT NULL REFERENCES uporabnik(id),
+            cas INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+            vsebina TEXT NOT NULL,
+            recept INTEGER NOT NULL REFERENCES recept(id)
+                        
+            );
+            """)
+    conn.commit()
+    
 
 def ustvari_recept():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS recept (
             id SERIAL PRIMARY KEY,
             ime TEXT NOT NULL,
-            opis TEXT NOT NULL,
+            opis TEXT,
             postopek TEXT NOT NULL,
-            datum DATE NOT NULL,
+            datum_objave DATE NOT NULL,
             ocena NUMERIC,
             cas TEXT,
-            rec_sestavina TEXT REFERENCES sestavina(id),
-            rec_vrsta TEXT REFERENCES vrsta(kategorija),
-            rec_uporabnik REFERENCES uporabnik(id),
-            rec_priloznost REFERENCES priloznost(priloznost),
-            rec_priprava REFERENCES priprava(priprava)
-            komentar TEXT
+            uporabnik INTEGER NOT NULL REFERENCES uporabnik(id),
+            komentar INTEGER REFERENCES komentar(id)
             
+            );
+            """)
+    conn.commit()
+    
+def ustvari_vrsta_recepta():
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS vsebuje (
+            recept INTEGER NOT NULL REFERENCES recept(id),
+            vrsta INTEGER REFERENCES vrsta(id),
+            PRIMARY KEY (recept, vrsta)
+            );
+            """)
+    conn.commit()
+    
+def ustvari_priloznost_recepta():
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS vsebuje (
+            recept INTEGER NOT NULL REFERENCES recept(id),
+            priloznost INTEGER REFERENCES priloznost(id),
+
+            PRIMARY KEY (recept, priloznost)
+            );
+            """)
+    conn.commit()
+
+def ustvari_priprava_recepta():
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS vsebuje (
+            recept INTEGER NOT NULL REFERENCES recept(id),
+            priprava INTEGER REFERENCES priprava(id),
+
+            PRIMARY KEY (recept, vrsta)
             );
             """)
     conn.commit()
@@ -79,10 +119,11 @@ def ustvari_recept():
 def ustvari_vsebuje():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS vsebuje (
-            količina NUMERIC,
-            recept INT
-            rec_sestavina TEXT REFERENCES sestavina(id),
-                        
+            količina NUMERIC NOT NULL,
+            recept INTEGER NOT NULL REFERENCES recept(id),
+            sestavina INTEGER NOT NULL REFERENCES sestavina(id),
+
+            PRIMARY KEY (recept, sestavina)
             );
             """)
     conn.commit()
@@ -91,13 +132,21 @@ def ustvari_vsebuje():
 
 def uvozi_podatke():
     #odpremo CSV datoteko
-    with open('recepti_data.csv', 'r', encoding='utf-8') as p:
+    with open('recepti.csv', 'r', encoding='utf-8') as p:
         vrstice = csv.reader(p)
         next(vrstice) # izpusti naslovno vrstico
-        for(ime, opis, uporabnik, datum, ID, priložnost, priprava, sezona, vrsta, sestavine, postopek) in vrstice:
+        for(ime, opis, uporabnik, datum, ID, priloznost, priprava, sezona, vrsta, cas, postopek, sestavine) in vrstice:
+            r = [None if x in ('', '-') else x for x in r]
+            r= r[1:(len(r))]
             cur.execute(
-                """INSERT INTO recept (ID, ime, opis, postopek, datum, cas) VALUES (?,?,?,?.?.?)"""
-                [ID, ime, opis, postopek, datum, cas])
+                """INSERT INTO priloznost (priloznost) VALUES (%s)
+                    RETURNING id""",r)
+            rid, = cur.fetchone()
         conn.commit()
             
             
+
+#od baze ze odvežeš conn.close()
+
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
